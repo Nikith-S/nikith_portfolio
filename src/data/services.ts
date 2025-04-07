23578b56d0eb2d375b0bb35fae804d1afa45fcaf
Nikith-S <nikith391@gmail.com>
2025-04-07 12:43:51 +0530

    new services added and updated

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,23 +1,9 @@
 import { IServiceItem } from "@/types";
 
 const services: IServiceItem[] = [
+
   {
     id: 1,
-    title: "Mobile App Development",
-    icons: [
-      "/skills/socket-io.png",
-      "/skills/dart.svg",
-      "/skills/flutter.svg",
-      "/skills/getx.png",
-      "/skills/firebase.svg",
-    ],
-    shortDescription:
-      "I create engaging mobile applications for your audience.",
-    description:
-      "I create captivating mobile apps from concept to deployment for iOS and Android. Using cutting-edge technologies, I ensure seamless performance, intuitive interfaces, and robust functionality that align with your business goals. Enjoy a flawless user experience and outstanding results.",
-  },
-  {
-    id: 2,
     title: "Web Development",
     icons: [
       "/skills/redux.svg",
@@ -31,7 +17,7 @@ const services: IServiceItem[] = [
       "I deliver stunning, user-friendly websites to establish your online presence. From simple sites to complex e-commerce platforms, I provide tailored solutions using the latest frameworks and technologies for a seamless, responsive, and SEO-friendly browsing experience. Enhance your online identity with quality.",
   },
   {
-    id: 3,
+    id: 2,
     title: "Backend Development",
     icons: [
       "/skills/socket-io.png",
@@ -45,7 +31,7 @@ const services: IServiceItem[] = [
       "I enhance digital applications with robust, scalable backend infrastructures. I develop efficient database structures, APIs, and configure servers for optimal performance, security, and scalability, ensuring your applications handle high traffic and complex data management seamlessly. Rely on strong backend solutions.",
   },
   {
-    id: 4,
+    id: 3,
     title: "Product Strategy",
     icons: [
       "/skills/git.svg",
@@ -60,7 +46,7 @@ const services: IServiceItem[] = [
       "I collaborate to define clear goals, target audiences, and a success roadmap. My expertise in product ideation and market analysis ensures your product meets user needs and aligns with your business strategy for long-term growth and full potential realization. Drive your product's success with strategic planning.",
   },
   {
-    id: 5,
+    id: 4,
     title: "DevOps",
     icons: [
       "/skills/docker.svg",
@@ -74,7 +60,7 @@ const services: IServiceItem[] = [
       "I streamline development and operations processes through effective DevOps practices. I implement continuous integration and deployment pipelines, manage cloud infrastructure, and use containerization to ensure efficient, reliable, and scalable software delivery. Improve your workflow with DevOps solutions.",
   },
   {
-    id: 6,
+    id: 5,
     title: "Database Management",
     icons: [
       "/skills/mysql.svg",
@@ -87,6 +73,26 @@ const services: IServiceItem[] = [
     description:
       "I manage and optimize your database systems for performance, reliability, and scalability. With expertise in SQL and NoSQL databases, I design schemas, write complex queries, and implement best practices for data integrity and security. Ensure your data is managed effectively and efficiently.",
   },
+
+  {
+    id: 6,
+    title: "AIML",
+    icons: [
+     
+      "/skills/pytourch.png",
+      "/skills/mongodb.svg",
+      "/skills/python.jpg",
+      "/skills/git.svg",
+      "/skills/sqlite.svg",
+    ],
+    shortDescription: "I design and implement AI/ML solutions to solve complex problems.",
+    description:
+      "I design and implement AI/ML solutions tailored to your business needs. With expertise in machine learning algorithms, deep learning frameworks, and data preprocessing techniques, I build predictive models, automate workflows, and extract actionable insights from data. Empower your business with cutting-edge AI-driven solutions to enhance decision-making and efficiency.",
+  },
+
+
+
+
 ];
 
 export default services;
