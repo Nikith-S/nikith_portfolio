408d43f46ccfd6e2c0ac983f36088d32d48f8d28
Nikith-S <nikith391@gmail.com>
2025-04-07 12:27:03 +0530

    url updated

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,38 +1,26 @@
 class Strings {
   static telegram = "Telegram";
-  static telegramLink = "https://telegram.me/nixrajput";
-  static telegramUsername = "nixrajput";
+  static telegramLink = "https://telegram.me/";
+  static telegramUsername = "nikith";
 
   static github = "GitHub";
-  static githubLink = "https://www.github.com/nixrajput";
-  static githubUsername = "nixrajput";
+  static githubLink = "https://github.com/Nikith-S";
+  static githubUsername = "Nikith-S";
 
   static linkedIn = "LinkedIn";
-  static linkedInLink = "https://www.linkedin.com/in/nixrajput";
-  static linkedInUsername = "nixrajput";
+  static linkedInLink = "https://www.linkedin.com/in/nikith-s-33366230b/";
+  static linkedInUsername = "Nikith";
 
   static instagram = "Instagram";
-  static instagramLink = "https://www.instagram.com/nixrajput";
-  static instagramUsername = "nixrajput";
-
-  static twitter = "Twitter";
-  static twitterLink = "https://www.twitter.com/nixrajput07";
-  static twitterUsername = "nixrajput07";
+  static instagramLink = "https://www.instagram.com/_nikith_s/";
+  static instagramUsername = "Nikith";
 
   static email = "Email";
   static primaryEmailLink = "mailto:nkr.nikhil.nkr@gmail.com";
   static primaryEmail = "nkr.nikhil.nkr@gmail.com";
 
-  static buyMeACoffee = "BuyMeACoffee";
-  static buyMeACoffeeLink = "https://buymeacoffee.com/nixrajput";
-  static buyMeACoffeeUsername = "nixrajput";
-
-  static koFi = "Ko-Fi";
-  static koFiLink = "https://ko-fi.com/nixrajput";
-  static koFiUsername = "nixrajput";
-
-  static fullName = "Nikhil Rajput";
-  static shortName = "nixrajput";
+  static fullName = "Nikith S";
+  static shortName = "nikith";
 }
 
 export default Strings;
